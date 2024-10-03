fae00ce6cc6269771a5acb0301518ea1ebb6c58d
SePuede <stale4669@gmail.com>
2024-10-03 23:51:53 +0200

    log in ( testing )

--- app/sign-in/page.js
+++ app/sign-in/page.js
@@ -1,7 +1,31 @@
+<<<<<<< HEAD
+'use client'
+ 
+import { useRouter } from 'next/navigation'
+=======
+>>>>>>> teamtrip/main
 import { CgPassword } from "react-icons/cg";
 
 export default function signIn(){
 
+<<<<<<< HEAD
+    function vai(){
+        event.preventDefault();
+        
+            let email = document.getElementById("email").value;
+            let passcode = document.f1.password.value;
+            const Cemail = "user@mail.com";
+            const Cpasscode = "1234";
+            
+            if(email == Cemail && passcode == Cpasscode){
+                document.getElementById("f1").submit();
+            }
+            
+        
+    }
+
+
+=======
     const handleSubmit = (event) => {
         
         const email = formData.get("email");
@@ -15,6 +39,7 @@ export default function signIn(){
 
     }
 
+>>>>>>> teamtrip/main
 
     return(
         <>
@@ -22,10 +47,18 @@ export default function signIn(){
                 <h2>
                     Login
                 </h2>
+<<<<<<< HEAD
+                <form id="f1" name="f1" method="POST" action="/">
+                    <input type="email" name="email" id="email" placeholder="email" size="30" required />
+                    <input type="password" name="password" id="password" placeholder="password" required />
+                    <input type="submit" value="submit" onClick={vai} />
+                    <h1 id="h1" name="h1" style={{visibility:"hidden"}}>Mangai</h1>
+=======
                 <form name="login" onSubmit={handleSubmit}>
                     <input type="email" id="email" placeholder="email" size="30" required />
                     <input type="password" id="password" placeholder="password" required />
                     <input type="submit" value="submit" />
+>>>>>>> teamtrip/main
                 </form>
                 
             </div>
@@ -49,6 +82,10 @@ export default function signIn(){
                 }
             `}
             </style>
+<<<<<<< HEAD
+
+=======
+>>>>>>> teamtrip/main
         </>
         
     )
